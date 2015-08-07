01451eca3698389ba1903834199cbb0cee3c478d
conrad1451 <Chansenq@gmail.com>
2015-08-07 05:04:04 -0400

    Update character.js

--- character.js
+++ character.js
@@ -64,7 +64,7 @@ THREEx.TinygigantiumChar	= function(skinUrl){
 	sizes.armW	=  4 * sizes.pixRatio;
 	sizes.armD	=  4 * sizes.pixRatio;
 	
-	var xPosi = -0.5;
+	var xPosi = 0;
 	var yPosi = 0.0;
 
 	// build model core hierachy
