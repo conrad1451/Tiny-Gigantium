85610f1bb8ff52e41b3cdebdb089b4c50a027451
conrad1451 <Chansenq@gmail.com>
2015-08-11 01:23:02 -0400

    Update character.js

--- character.js
+++ character.js
@@ -21,7 +21,7 @@ THREEx.TinygigantiumChar	= function(skinUrl){
 	// set default arguments values
 	// link works ! -->  http://i1205.photobucket.com/albums/bb424/cman_was_here/theflash_zpsbohbmk1n.png
 	
-	skinUrl	= skinUrl || ('http://i1205.photobucket.com/albums/bb424/cman_was_here/theflash%20-%20Copy_zpscprmcdrl.png')
+	skinUrl	= skinUrl || ('http://i1205.photobucket.com/albums/bb424/cman_was_here/theflash_zpsbohbmk1n.png')
 
 	//////////////////////////////////////////////////////////////////////////////////
 	//		comment								//
