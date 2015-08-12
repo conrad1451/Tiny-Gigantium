23ab8e4d7fdacd599f83bad7de9bfeb50817513d
conrad1451 <Chansenq@gmail.com>
2015-08-12 00:42:30 -0400

    Update bodymove.js

--- bodymove.js
+++ bodymove.js
@@ -95,13 +95,13 @@ THREEx.TinygigantiumCharBodyAnimations	= function(character){
 	// Setup 'walkbackwards' animation
 	var angleRange	= Math.PI/3-Math.PI/10;
 	animations.add('walkbackwards'	, THREEx.createAnimation().pushKeyframe(0.5, {
-		armLRotationZ	: -Math.PI/30,
-		armRRotationZ	: +Math.PI/30,
+		armLRotationZ	: +Math.PI/30,
+		armRRotationZ	: -Math.PI/30,
 		armRotationX	: -angleRange,
 		legRotationX	: +angleRange		
 	}).pushKeyframe(0.5, {
-		armLRotationZ	: -Math.PI/30,
-		armRRotationZ	: +Math.PI/30,
+		armLRotationZ	: +Math.PI/30,
+		armRRotationZ	: -Math.PI/30,
 		armRotationX	: +angleRange,
 		legRotationX	: -angleRange
 	}).propertyTweens(propTweens).onCapture(onCapture).onUpdate(onUpdate));
