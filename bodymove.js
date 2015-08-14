36489bd5ba279b96ca121854c854691728ec3a73
conrad1451 <Chansenq@gmail.com>
2015-08-13 20:59:37 -0400

    Update bodymove.js

--- bodymove.js
+++ bodymove.js
@@ -80,14 +80,14 @@ THREEx.TinygigantiumCharBodyAnimations	= function(character){
 		animations.add('strafeRight'	, THREEx.createAnimation().pushKeyframe(0.5, {
 		armLRotationZ	: +angleRange/2,
 		armRRotationZ	: -angleRange/2,
-		armRotationX	: +Math.PI/10,
+		armRotationX	: -Math.PI/10,
 		legLRotationZ	: -angleRange,
 		legRRotationZ	: +angleRange,
 		legRotationX	: -Math.PI/5		
 	}).pushKeyframe(0.5, {
 		armLRotationZ	: -angleRange/2,
 		armRRotationZ	: +angleRange/2,
-		armRotationX	: +Math.PI/10,
+		armRotationX	: -Math.PI/10,
 		legLRotationZ	: +angleRange,
 		legRRotationZ	: -angleRange,
 		legRotationX	: -Math.PI/5
